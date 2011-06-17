cf5ba56e0fc202e23dd62c43cd6c7a87e9459ff2
Steve Newell <newellista@gmail.com>
2011-06-17 14:27:07 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+gem 'haml'
+gem 'capistrano'
+gem 'capistrano-ext'
+gem 'compass'
+gem 'will_paginate'
+
+group :development do
+  gem "ruby-debug19", :require => 'ruby-debug'
+  gem "getopt"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scratch::Application.config.secret_token = 'ba205ab6348021b31eb42f03ff80ed6c66f280d356dd764bc04ca1b613e7e3f8401405efa49bd1a0af337062fe6d12591220efaec5cf2375808f8dd87077e2b1'
